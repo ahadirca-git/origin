e5c0b95a93843cba414db2fa8e82d65a99989398
Alexandr Hadirca <ahadirca@noction.com>
2024-09-04 16:10:51 +0300

    Python  ( update )deleted unused script

--- Python/CH_insert_report_data.py
+++ Python/CH_insert_report_data.py
@@ -5,5 +5,4 @@ counts =5000
 for i in range (1,counts):
     time_past_previous_5_min = 300*i
     ts= datetime.fromtimestamp(timestamp - time_past_previous_5_min - timestamp % 300)
-    # print (ts)
     print("insert into irp.report_data_5m (ts, id_report, id_data, data_int ) values ('%s', 2, 14, 1234);" %(ts))
